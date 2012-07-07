4078bf104693e0f8432d61e7b2ef305af9ac9159
Ryan ORourke <rorourke@marchex.com>
2012-07-07 14:30:15 -0700

    Postgre I hate you.

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -1,62 +1,58 @@
-# Tasks schema aa
-
 # --- !Ups
 
 CREATE TABLE league (
-  id bigint PRIMARY KEY AUTO_INCREMENT,
+  id bigserial PRIMARY KEY,
   league_name varchar(255),
   location varchar(255),
   description varchar(255),
-  active boolean DEFAULT 0
+  active boolean DEFAULT false
 );
 
 CREATE TABLE season (
-  id bigint PRIMARY KEY AUTO_INCREMENT,
+  id bigserial PRIMARY KEY,
   league_id bigint NOT NULL,
   start_date date NOT NULL,
   completed boolean DEFAULT FALSE,
-  weeks_regular tinyint DEFAULT 1,
-  weeks_playoffs tinyint DEFAULT 1,
-  byes tinyint DEFAULT 0,
-  doubleheaders tinyint DEFAULT 0,
+  weeks_regular smallint DEFAULT 1,
+  weeks_playoffs smallint DEFAULT 1,
+  byes smallint DEFAULT 0,
+  doubleheaders smallint DEFAULT 0,
   FOREIGN KEY (league_id) REFERENCES league(id) ON DELETE CASCADE
 );
 
 CREATE TABLE week (
-  id bigint PRIMARY KEY AUTO_INCREMENT,
+  id bigserial PRIMARY KEY,
   season_id bigint NOT NULL,
-  game_date datetime NOT NULL,
+  game_date timestamp NOT NULL,
   playoff boolean,
   FOREIGN KEY (season_id) REFERENCES season(id) ON DELETE CASCADE
 );
 
 CREATE TABLE team (
-  id bigint PRIMARY KEY AUTO_INCREMENT,
+  id bigserial PRIMARY KEY,
   name varchar(255),
   captain_name varchar(255),
   captain_email varchar(200)
 );
 
 CREATE TABLE game (
-  id bigint PRIMARY KEY AUTO_INCREMENT,
+  id bigserial PRIMARY KEY,
   week_id bigint NOT NULL,
   start_time time NOT NULL,
-  court tinyint,
+  court smallint,
   team1_id bigint,
   team2_id bigint,
-  num_sets tinyint,
+  num_sets smallint,
   FOREIGN KEY (week_id) REFERENCES week(id) ON DELETE CASCADE,
   FOREIGN KEY (team1_id) REFERENCES team(id),
   FOREIGN KEY (team2_id) REFERENCES team(id)
-
 );
 
 CREATE TABLE set (
-  num tinyint,
+  num smallint,
   game_id bigint,
-  team1_score tinyint,
-  team2_score tinyint,
-
+  team1_score smallint,
+  team2_score smallint,
   PRIMARY KEY (game_id, num),
   FOREIGN KEY (game_id) REFERENCES game(id) ON DELETE CASCADE
 );
